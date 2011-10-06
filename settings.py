1361bb4784453a4aa1c0c33291afc35d9649fbb1
Василий Большаков <va.bolshakov@gmail.com>
2011-10-06 15:01:17 +0400

    files url changed to subdomain

--- settings.py
+++ settings.py
@@ -78,7 +78,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -225,5 +224,8 @@ if DEBUG:
     
     STATICFILES_DIRS += (os.path.join(PROJECT_DIR, 'media'),)
 else:
+    FILES_URL = 'http://files.e-cigar.ru'
+    MEDIA_URL = FILES_URL + MEDIA_URL
+    STATIC_URL = FILES_URL + STATIC_URL
     TEMPLATE_LOADERS = (
         ('django.template.loaders.cached.Loader', TEMPLATE_LOADERS),)
