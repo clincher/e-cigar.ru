25b728cb18331d941ae454bb7fdf7741b46cf2b7
Василий Большаков <va.bolshakov@gmail.com>
2011-10-13 13:00:29 +0400

    added wysiwyg to banner admin model

--- apps/banners/admin.py
+++ apps/banners/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from models import Banner
 
+class BannerAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('tiny_mce/tiny_mce.js',
+              'filebrowser/js/TinyMCEAdmin.js',)
 
-admin.site.register(Banner)
+
+admin.site.register(Banner, BannerAdmin)
