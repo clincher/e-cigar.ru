b7d418a4d7faeb6cccf497b47dd2ec816ab21c63
clincher <va.bolshakov@gmail.com>
2012-02-24 00:23:33 +0400

    feedback fixed

--- urls.py
+++ urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     (r'^tinymce/', include('tinymce.urls')),
     (r'^grappelli/', include('grappelli.urls')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^feedback/', include('feedback.urls')),
+    (r'^feedback/', include('apps.feedback.urls')),
     (r'^callback/', include('apps.callback.urls')),
     url(r'^captcha/', include('captcha.urls')),
 
