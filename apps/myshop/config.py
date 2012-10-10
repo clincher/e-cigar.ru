f39bb2ca16bab70b6c03c85f2b312d46ac7e585f
clincher <va.bolshakov@gmail.com>
2012-10-11 01:36:11 +0400

    help_text fixed

--- apps/myshop/config.py
+++ apps/myshop/config.py
@@ -13,31 +13,31 @@ config_register(StringValue(
     SHOP_GROUP,
     'MOST_DISCOUNT_CIGARETTE',
     description = u'Самое выгодное предложение',
-    help_text = u'Укажите численный идентификатор продукта (можно посмотреть в'
-                u' строке адреса',
+    help_text = u'Укажите метку сигареты (можно посмотреть на странице'
+                u' редактирования сигареты',
     default = 'turbo-white'
 ))
 config_register(StringValue(
     SHOP_GROUP,
     'MOST_POPULAR_CIGARETTE',
     description = u'Самая популярная сигарета',
-    help_text = u'Укажите численный идентификатор продукта (можно посмотреть в'
-                u' строке адреса',
+    help_text = u'Укажите метку сигареты (можно посмотреть на странице'
+                u' редактирования сигареты',
     default = 'black'
 ))
 config_register(StringValue(
     SHOP_GROUP,
     'BEST_MALE_CIGARETTE',
     description = u'Лучшая мужская сигарета',
-    help_text = u'Укажите численный идентификатор продукта (можно посмотреть в'
-                u' строке адреса',
+    help_text = u'Укажите метку сигареты (можно посмотреть на странице'
+                u' редактирования сигареты',
     default = 'curage'
 ))
 config_register(StringValue(
     SHOP_GROUP,
     'BEST_FEMALE_CIGARETTE',
     description = u'Лучшая женская сигарета',
-    help_text = u'Укажите численный идентификатор продукта (можно посмотреть в'
-                u' строке адреса',
+    help_text = u'Укажите метку сигареты (можно посмотреть на странице'
+                u' редактирования сигареты',
     default = 'sx2'
 ))
